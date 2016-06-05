973564c357a14ac8d088eab91a2a4bb7418d98d4
Sandbox Sync <support@getsandbox.com>
2016-06-05 00:13:37 -0400

    Changes

--- main.js
+++ main.js
@@ -90,20 +90,3 @@ Sandbox.define('/upload/orders/washafter', 'POST', upload.uploadWashAfter);
 
 
 
-Sandbox.define('/api/Customers/Balance','GET', function(req, res) {
-    // Check the request, make sure it is a compatible type
-    if (!req.is('application/json')) {
-        return res.send(400, 'Invalid content type, expected application/json');
-    }
-    
-    // Set the type of response, sets the content type.
-    res.type('application/json');
-    
-    // Set the status code of the response.
-    res.status(200);
-    
-    // Send the response body.
-    res.json(
-        1
-    );
-})
